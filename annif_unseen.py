0dc6356cbbb2860885c5e3a5eacc05f99096a748
Antony Owino <owino@ualberta.ca>
2020-05-19 16:40:23 -0600

    Update Annif Batch Indexer
    Signed-off-by: Antony Owino <owino@ualberta.ca>

--- annif_unseen.py
+++ annif_unseen.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
+import os
+import re
+import sys
+import glob
+import argparse
+from pathlib import Path
 from pprint import pprint
+from xml.dom import minidom
 import xml.etree.cElementTree as ET
-from types import SimpleNamespace
 
 import annif
-import annif.corpus
 import annif.eval
+import annif.corpus
 import annif.project
+import annif.backend
 from annif.project import Access
 from annif.corpus.skos import SubjectFileSKOS
 from annif.suggestion import SubjectSuggestion, SuggestionResult, \
     LazySuggestionResult, ListSuggestionResult, SuggestionFilter
 from annif.corpus import SubjectIndex
-import annif.backend
-from sklearn.feature_extraction.text import TfidfVectorizer
-import xml.etree.cElementTree as ET
-from xml.dom import minidom
-
-# text = open('data/TXT_PROC/1975/dds-89398-page-8-article-01.txt', "r").read().lower()
-text = open('data/TXT_PROC/1975/dds-89407-page-8-article-10.txt', "r").read()
 
 class DotDict(dict):
     pass
@@ -39,23 +39,76 @@ backend = backend_type(
     config_params={'limit': 5},
     project=project
 )
+    
+def append_suggestions_node(results, txt_xml_path):
+    # https://stackoverflow.com/questions/28782864/modify-xml-file-using-elementtree
+    tree = ET.ElementTree(file=txt_xml_path)
+    root = tree.getroot()
+    
+    #subjects = ET.SubElement(root, "subjects")
+    subjects = ET.Element('subjects')
+    for result in results:
+        pprint(result, indent=2)
+        ET.SubElement(subjects, "subject", score=str(result.score), uri=str(result.uri)).text = result.label
 
-results = backend.suggest(text, project)
+    root.insert(1, subjects)
 
-# https://stackoverflow.com/questions/28782864/modify-xml-file-using-elementtree
-tree = ET.ElementTree(file='dds-89407-page-8-article-10.xml')
-root = tree.getroot()
+    xmlstr = ET.tostring(root).decode()
+    xmlstr = minidom.parseString(xmlstr).toprettyxml(indent="", newl="")
+    return xmlstr
+                
+def main(args):
+    # get params
+    proc_year = args.year
+    
+    prj_root = os.getcwd()
+    #prj_root = os.path.join(current_directory, rf'{output_name}')
+    #prj_root = os.path.dirname(current_directory)
+    data_dir = f'{prj_root}/data'
+    txt_proc_dir = f'{data_dir}/TXT_PROC'
+    txt_xml_dir = f'{data_dir}/TXT_XML'
+    xml_dir = f'{data_dir}/XML'    
+    
+    path_list = []
+    for f in sorted(Path(txt_proc_dir).glob(f'{proc_year}/*.txt')):
+        txt_path = str(f) # cast PosixPath to str
+        txt_name = os.path.basename(txt_path)
+        path_list.append(txt_name)
 
-#subjects = ET.SubElement(root, "subjects")
-subjects = ET.Element('subjects')
-for result in results:
-    pprint(result, indent=2)
-    ET.SubElement(subjects, "subject", score=str(result.score), uri=str(result.uri)).text = result.label
+    if not os.path.exists(f"{xml_dir}/{proc_year}"):
+        os.makedirs(f"{xml_dir}/{proc_year}")
 
-root.insert(1, subjects)
+    for idx, path in enumerate(path_list):
+        txt_name = os.path.basename(path)
+        txt_sans_ext = os.path.splitext(txt_name)[0]
+        txt_proc_path = f"{txt_proc_dir}/{proc_year}/{txt_name}"
+        txt_xml_path = f"{txt_xml_dir}/{proc_year}/{txt_sans_ext}.xml"
+        xml_path = f"{xml_dir}/{proc_year}/{txt_sans_ext}.xml"
 
-xmlstr = ET.tostring(root).decode()
-xmlstr = minidom.parseString(xmlstr).toprettyxml(indent="", newl="")
+        # only handle valid files i.e. files with useful size of text
+        if os.stat(txt_proc_path).st_size > 50: # more 50 bytes at least
+            with open(txt_proc_path, 'r', encoding='utf-8') as infile:
+                whole_text = infile.read()
 
-with open("filename.xml",'w') as outfile:
-    outfile.write(xmlstr)
+                results = backend.suggest(whole_text, project)
+
+                xmlstr = append_suggestions_node(results, txt_xml_path)
+
+                with open(xml_path,'w') as outfile:
+                    outfile.write(xmlstr)
+                    print(f"{xml_path} DONE!")
+                
+
+if __name__ == '__main__':
+    # Instantiate the parser
+    parser = argparse.ArgumentParser(prog="Annifier", description='Suggest Subject and Append Subject into Custom XML')
+    parser.add_argument('--year', type=str, help='Year to process') # Optional argument
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+    args = parser.parse_args()
+
+    if not args.year:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    # execute only if run as the entry point into the program
+    main(args)                
